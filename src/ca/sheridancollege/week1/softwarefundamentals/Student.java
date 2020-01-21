d586cd47127d570497c3a9a5c7f4b51c1118c54c
Asad_Danish <Asad_Danish@10.48.19.17>
2020-01-21 17:38:06 -0500

    another change

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -10,9 +10,20 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * @author sidhu376
  */
 public class Student {
-    
-    private String name;
 
+	private String name;
+	private int Id;
+	private String grade;
+	private double GPA;
+	
+    
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int Id) {
+		this.Id = Id;
+	}
     /**
      * Get the value of name
      *
@@ -30,6 +41,34 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+
+	/**
+	 * @return the grade
+	 */
+	public String getGrade() {
+		return grade;
+	}
+
+	/**
+	 * @param grade the grade to set
+	 */
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	/**
+	 * @return the GPA
+	 */
+	public double getGPA() {
+		return GPA;
+	}
+
+	/**
+	 * @param GPA the GPA to set
+	 */
+	public void setGPA(double GPA) {
+		this.GPA = GPA;
+	}
 
 }
  
